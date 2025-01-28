ede485e0e3251eea884f8443d61d0fb44c5b4280
JAVIER ANTONIO ROJAS MORERA <208610800@castrocarazo.ac.cr>
2025-01-27 21:16:41 -0600

    Trabajo con conexion sql

---
+++ Models/Contacto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Contacto
+    {
+        public int  Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio.")]
+        public string Mensaje { get; set; }
+    }
+}
