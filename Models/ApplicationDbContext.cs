f7caa3251b5c6ec95ecf4e7bf6519d712481a8b2
Javier Rojas Morera <javiantonio07@gmail.com>
2025-02-03 20:15:39 -0600

    Program.cs

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApplication1.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
 {
     public class ApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                    : base(options) { }
+
+
+    public DbSet<ContactMessage> ContactMessages { get; set; }
     }
 }
